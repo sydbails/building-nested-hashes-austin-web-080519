97cb159c0a00db7f8b89ac3cbda5ac294b5492d5
Syd Bailey <sydabailey@gmail.com>
2019-08-05 03:26:59 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -8,7 +8,7 @@ def third_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"
         },
       :hero_friends => []
-   }, 
+   },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"
         },
@@ -20,5 +20,5 @@ def third_challenge
    }
   }
 
-  
+
 end
